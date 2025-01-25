fa63e38e6b85886323a61b7ee32189a784e77e60
Ryan Drake <radrake1106@gmail.com>
2025-01-25 12:56:56 -0600

    Add basic weapon and projectile, Update input mapping

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -8,7 +8,7 @@ func _physics_process(_delta):
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_vector("Left", "Right", "Up", "Down")
+	var direction = Input.get_vector("left", "right", "up", "down")
 	if direction:
 		velocity = direction * SPEED
 	else:
