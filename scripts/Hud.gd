e05062d1ec961da90d2f4092bc2c41ba3751c732
Ryan Drake <radrake1106@gmail.com>
2025-01-25 21:25:16 -0600

    Add grenade cooldown to hud, Add enemy physics layer

--- scripts/Hud.gd
+++ scripts/Hud.gd
@@ -1,16 +1,16 @@
 extends CanvasLayer
 
 @onready var grenade_controller: Node2D = $"../GrenadeController"
-@onready var grenade_cd: ColorRect = $PanelContainer/MarginContainer/VBoxContainer/GrenadeCD
-@onready var grenade_cd_timer: Timer = $PanelContainer/MarginContainer/VBoxContainer/GrenadeCD/GrenadeCDTimer
+@onready var grenade_cd: ColorRect = $PanelContainer/MarginContainer/VBoxContainer/ReferenceRect/GrenadeCD
+@onready var grenade_cd_timer: Timer = $PanelContainer/MarginContainer/VBoxContainer/ReferenceRect/GrenadeCD/GrenadeCDTimer
 
 
+func _process(delta: float) -> void:
+	if !grenade_cd_timer.is_stopped():
+		print(grenade_cd_timer.time_left)
+		grenade_cd.scale.y = 1 - (grenade_cd_timer.time_left / (300 / 60))
+		pass
 
-#func _process(delta: float) -> void:
-	#if !grenade_cd_timer.is_stopped():
-		#print(grenade_cd_timer.time_left)
-		#grenade_cd.scale.y = 300
-		#pass
 
-#func _on_grenade_controller_grenade_thrown() -> void:
-	#grenade_cd_timer.start(5)
+func _on_grenade_controller_grenade_thrown() -> void:
+	grenade_cd_timer.start(300 / 60)
