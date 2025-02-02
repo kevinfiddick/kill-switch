48f527ca5d9d46f3bdb8b8b7970383234f03b113
Ryan Drake <radrake1106@gmail.com>
2025-02-02 17:47:11 -0600

    Add/fix scene transitions

--- scripts/menus/MainMenu.gd
+++ scripts/menus/MainMenu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/TestLevel.tscn")
+	get_tree().change_scene_to_file("res://scenes/levels/bedroom/Bedroom.tscn")
 
 
 func _on_quit_pressed() -> void:
