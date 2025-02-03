481bb71fb0cd09ae5799c29adfc58bd7dfd38905
Ryethegret <65608840+Ryethegret@users.noreply.github.com>
2025-02-03 02:46:52 +0000

    main menu music

--- scripts/menus/MainMenu.gd
+++ scripts/menus/MainMenu.gd
@@ -1,7 +1,15 @@
 extends Control
 
 
+@onready var main_menu_music: AudioStreamPlayer = $"Main Menu Music"
+@onready var main_menu_music_end: AudioStreamPlayer = $"Main Menu Music End"
+
+func _ready() -> void:
+	main_menu_music.play()
+
 func _on_play_pressed() -> void:
+	main_menu_music_end.play()
+	main_menu_music.stop()
 	get_tree().change_scene_to_file("res://scenes/levels/bedroom/Bedroom.tscn")
 
 
